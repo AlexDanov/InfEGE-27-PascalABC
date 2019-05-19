f9429fb6c93dc1864933e0b77e9d7b14466ac0da
Alex Danov <50717668+AlexDanov@users.noreply.github.com>
2019-05-19 19:33:07 +0300

    Add files via upload

---
+++ Поляков 27-57 4b ИнформатикБУ стрим#57.pas
@@ -0,0 +1,14 @@
+﻿var i,n,x,R,mc,mn:integer;
+begin
+  read(n);
+  mc:=0; mn:=0;
+  for i:=1 to n do begin
+    read(x);
+    if (x mod 2 = 0) and (x>mc) then mc:=x;
+    if (x mod 2 <>0) and (x>mn) then mn:=x;
+  end;
+  if mc*mn>0 then R := mc+mn
+  else R := -1;
+  writeln(R);
+end.
+    
