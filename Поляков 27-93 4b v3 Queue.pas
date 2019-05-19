1a358333aa5d38735a5eedbbe390fbf12e5d3fde
Alex Danov <50717668+AlexDanov@users.noreply.github.com>
2019-05-19 18:56:00 +0300

    Add files via upload

---
+++ Поляков 27-93 4b v3 Queue.pas
@@ -0,0 +1,17 @@
+﻿const s=6;
+var n,i,R,t:integer;
+    k:array[0..9] of integer=(0,0,0,0,0,0,0,0,0,0); //счетчики
+    b:=new Queue<integer>; // очередь
+begin
+  read(n);
+  for i:=0 to s-1 do b.Enqueue(ReadInteger() mod 10);
+  R:=0;
+  for i:=s+1 to n do begin
+    k[b.Dequeue()]+=1;
+    t := ReadInteger() mod 10;
+    if t in [1,3] then R+=k[4-t];
+    if t in [7,9] then R+=k[16-t];
+    b.Enqueue(t);
+  end;
+  writeln(R);
+end.
