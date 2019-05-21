1dfb0e73ac9615edf6301b301aedd45a474e3d69
Alex Danov <50717668+AlexDanov@users.noreply.github.com>
2019-05-21 20:05:20 +0300

    Add files via upload

--- Поляков 27-65 4b.pas
+++ Поляков 27-65 4b.pas
@@ -1,1 +1,16 @@
-﻿
+﻿var n,i,a,b,s,rmin,r:integer;
+begin
+  s:=0;
+  rmin:=10001;
+  read(n);
+  for i:=1 to n do begin
+    read(a,b);
+    s += max(a,b);
+    r := abs(a-b);
+    if (r mod 4<>0)and(r<rmin) then rmin := r;
+  end;
+  if s mod 4 = 0 then 
+    if rmin<>10001 then s-=rmin
+    else s := 0;
+  writeln(s);
+end.
