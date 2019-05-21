2289bc7bf79a8d096c53fc998a612a8dd9526964
Alex Danov <50717668+AlexDanov@users.noreply.github.com>
2019-05-21 20:02:32 +0300

    Add files via upload

---
+++ Поляков 27-87 4b ИнформатикБУ стрим#45.pas
@@ -0,0 +1,12 @@
+﻿var i,n,maxp,min,x:integer;
+begin
+  read(n,min);
+  maxp := -10000;
+  for i:=2 to n do begin
+    read(x);
+    if (x-min>maxp) then maxp := x-min;
+    if (x<min) then min := x;
+  end;
+  writeln(maxp);
+end.
+// 9 10 2 5 4 8 7 1 6 4 = 6
