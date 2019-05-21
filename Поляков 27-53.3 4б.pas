46dbfaa69bb3710ec6ea550e37368db870291ca9
Alex Danov <50717668+AlexDanov@users.noreply.github.com>
2019-05-21 18:58:25 +0300

    Add files via upload

---
+++ Поляков 27-53.3 4б.pas
@@ -0,0 +1,13 @@
+﻿var n,i,a,b,t,ta,tb,tmin:integer;
+begin
+  read(n,t);
+  (ta,tb):=(0,t);
+  for i:=1 to n do begin
+    read(a,b,t);
+    ta:=ta+a; tb:=tb+b;
+    if tb>ta+t then tb:=ta+t;
+    if ta>tb+t then ta:=tb+t;
+  end;
+  tmin := min(tb,ta+t);
+  writeln(tmin);
+end.
---
+++ Поляков 27-53.4 4б.pas
@@ -0,0 +1,11 @@
+﻿var n,i,sa,sb,sc,a,b,c:integer;
+begin
+  read(n,sa,sb,sc);
+  for i:=2 to n do begin
+    read(a,b,c);
+    (sa,sb,sc) := (max(sa,sb)+a,
+                   max(sa,max(sb,sc))+b,
+                   max(sb,sc)+c);
+  end;
+  writeln(max(sa,max(sb,sc)));
+end.
