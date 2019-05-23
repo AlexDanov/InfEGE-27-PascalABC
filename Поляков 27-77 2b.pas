049d1ae0b2a631c13f11a94e1146313f378f2334
Alex Danov <50717668+AlexDanov@users.noreply.github.com>
2019-05-23 16:54:14 +0300

    Add files via upload

---
+++ Поляков 27-77 2b.pas
@@ -0,0 +1,11 @@
+﻿var a:array[1..10000] of integer;
+    n,i,j,k:integer;
+begin
+  read(n);
+  for i:=1 to n do read(a[i]);
+  k:=0;
+  for i:=1 to n-1 do 
+    for j:=i+1 to n do 
+      if (a[i]+a[j])mod 12=0 then k+=1;
+  writeln(k);
+end.
