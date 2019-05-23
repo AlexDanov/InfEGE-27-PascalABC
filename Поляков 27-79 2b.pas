86f45381499734da57905095efe8513d297be336
Alex Danov <50717668+AlexDanov@users.noreply.github.com>
2019-05-23 17:04:40 +0300

    Add files via upload

--- Поляков 27-79 2b.pas
+++ Поляков 27-79 2b.pas
@@ -1,1 +1,12 @@
-﻿
+﻿var a:array[1..10000] of integer;
+    n,i,j,k,s:integer;
+begin
+  read(n);
+  for i:=1 to n do read(a[i]);
+  s:=0;
+  for i:=1 to n-1 do 
+    for j:=i+1 to n do 
+      for k:=j+1 to n do 
+        if (a[i]+a[j]+a[k])mod 12=0 then s+=1;
+  writeln(s);
+end.
---
+++ Поляков 27-79 4b.pas
@@ -0,0 +1,19 @@
+﻿var s:array[0..11] of integer=(0,0,0,0,0,0,0,0,0,0,0,0);
+    n,i,j,k,x:integer;
+begin
+  read(n);
+  for i:=1 to n do 
+    s[ReadInteger() mod 12] += 1;   
+  k:=0;
+  for i:=0 to 11 do
+    for j:=i to 11 do begin
+      k:=(24-i-j) mod 12;
+      if (i=j) and (j=k) then k+=s[i]*(s[i]-1)*(s[i]-2) div 6
+      else if i=j then k+=s[k]*s[i]*(s[i]-1) div 2
+      else if i=k then k+=s[j]*s[i]*(s[i]-1) div 2
+      else if k=j then k+=s[i]*s[k]*(s[k]-1) div 2
+      else k+=s[i]*s[j]*s[k];
+    end;
+  writeln(k);
+end.
+      
