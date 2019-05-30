ca91d6d9a39f917e8ea7c45fefd0b1fdaffb56ae
Alex Danov <50717668+AlexDanov@users.noreply.github.com>
2019-05-30 18:55:57 +0300

    Add files via upload

--- Поляков 27-106 4b v2.pas
+++ Поляков 27-106 4b v2.pas
@@ -6,7 +6,7 @@ begin
     if (b=c)or(b=d) then begin
       L+=1;
       if b=d then swap(c,d);
-    end else if (a=c)or(a=b) then begin
+    end else if (a=c)or(a=d) then begin
       L:=2;
       if a=d then swap(c,d);
     end else L:=1;
@@ -16,3 +16,4 @@ begin
   writeln(Lmax);
 end.
 // 5  1 2  2 3  5 4  5 5  5 1  => 3
+//28 5 1 6 6 7 7 3 3 2 3 1 7 1 3 6 3 5 6 3 5 2 7 4 6 3 7 2 2 6 1 4 7 6 2 1 1 4 2 7 5 4 3 4 1 1 2 4 4 2 5 5 4 5 5 6 7 => 5
