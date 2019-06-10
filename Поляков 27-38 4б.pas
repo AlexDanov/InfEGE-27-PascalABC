6214bd484b8b8e1877eb11db2bc3c7da55f28f2f
Alex Danov <50717668+AlexDanov@users.noreply.github.com>
2019-06-10 13:47:49 +0300

    Add files via upload

---
+++ Поляков 27-38 4б.pas
@@ -0,0 +1,15 @@
+﻿var a,b,h0,h,hmax:integer;
+begin
+  read(a,b);
+  h0 :=a; h:=0; hmax:=0;
+  while b>0 do begin
+    if a<b then begin
+      h:=b-h0;
+      if h>hmax then hmax:=h;
+    end else
+      h0:=b;
+    a:=b; read(b);
+  end;
+  writeln(hmax);
+end.
+// 5 6 5 4 3 2 1 2 3 2 3 4 5 6 7 8 0
